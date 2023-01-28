f216036b1bcb14c2126636e4638bf0567ad0374b
rnakai <rnakai@student.42tokyo.jp>
2023-01-28 21:35:49 +0900

    old date formatもcsvに流し込めるように

--- parser.rb
+++ parser.rb
@@ -6,13 +6,24 @@ def main
   old_date_fmt_filename = './anime_list/old_date.txt'
   current_fmt_filename = './anime_list/current.txt'
 
-  CSV.open('result.csv', 'w') do |csv|
+  output_filename = 'result.csv'
+  # output_filename = 'test.csv'
+
+  CSV.open(output_filename, 'w') do |csv|
     csv << %w[title start_date end_date status dropped_episode other]
 
     File.open(no_date_fmt_filename, 'r') do |f|
+      pattern = %r|^(?<title>.*?)\s*(?<start_date>\d{1,4}/\d)?$|
+      f.each do |line|
+        info = pattern.match(line)
+        csv << [info[:title], info[:start_date]]
+      end
     end
 
     File.open(old_date_fmt_filename, 'r') do |f|
+      f.each do |line|
+        csv << convert_old_date_fmt2csv(line)
+      end
     end
 
     File.open(current_fmt_filename, 'r') do |f|
@@ -20,6 +31,30 @@ def main
         csv << convert_current_fmt2csv(line)
       end
     end
+  end
+end
+
+def convert_old_date_fmt2csv(line)
+  date_pattern = %r|\d{2}/\d{1,2}/\d{1,2}|
+  basic_pattern = /^(?<title>.*?)\s*(?<start_date>#{date_pattern})/
+
+  # タイトル 20/9/12 ~ 20/10/1
+  range_pattern = /#{basic_pattern}\s?~\s?(?<end_date>#{date_pattern})$/
+
+  # タイトル 20/9/12;
+  oneday_pattern = /#{basic_pattern};$/
+
+  # タイトル 20/9/12
+  start_only_pattern = /#{basic_pattern}$/
+
+  if (info = range_pattern.match(line))
+    [info[:title], info[:start_date], info[:end_date], 'complete', nil, nil]
+  elsif (info = oneday_pattern.match(line))
+    [info[:title], info[:start_date], info[:start_date], 'complete', nil, nil]
+  elsif (info = start_only_pattern.match(line))
+    [info[:title], info[:start_date], nil, 'complete', nil, nil]
+  else
+    [nil, nil, nil, nil, nil, line]
   end
 end
 
